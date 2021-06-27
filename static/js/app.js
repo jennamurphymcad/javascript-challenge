3b4acdeb9e7d035f5393e3a36aa6df03aed409c2
Jenna Murphy <jennadeemurphy@gmail.com>
2021-06-27 17:35:02 -0500

    moved index for ufo-level-1 into folder. Many attempts at level two but strugglinggit add . :(

--- static/js/app.js
+++ static/js/app.js
@@ -8,6 +8,7 @@ var tbody = d3.select("tbody");
 
 // Select the button
 var button = d3.select("#filter-btn");
+var reset = d3.select('#reset-btn');
 
 // Select the form
 var form = d3.select("#form");
@@ -15,6 +16,7 @@ var form = d3.select("#form");
 // Create event handlers 
 button.on("click", runFilter);
 form.on("submit", runFilter);
+reset.on("click", runReset);
 
 
 // Append all the UFO data to the html table when page is loaded
@@ -33,20 +35,42 @@ function runFilter() {
     d3.event.preventDefault();
     
     // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
+    var dateInputElement = d3.select("#datetime");
+    // var cityInputElement = d3.select("#cityFilter");
+    // var stateInputElement = d3.select("#stateFilter");
+    // var countryInputElement = d3.select("#countryFilter");
+    // var shapeInputElement = d3.select("#shapeFilter");
   
     // Get the value property of the input element
-    var inputValue = inputElement.property("value");
+    var dateInputValue = dateInputElement.property("value");
+    // var cityInputValue = cityInputElement.property("value");
+    // var stateInputValue = stateInputElement.property("value");
+    // var countryInputValue = countryInputElement.property("value");
+    // var shapeInputValue = shapeInputElement.property("value");
+// 
+    var dateFilteredData = tableData.filter(tableData => tableData.datetime === dateInputValue);
+    // var cityFilteredData = tableData.filter(tableData => tableData.city === cityInputValue);
+    // var stateInputData = tableData.filter(tableData => tableData.state === stateInputValue);
+    // var countryInputData = tableData.filter(tableData => tableData.country === countryInputValue);
+    // var shapeInputData = tableData.filter(tableData => tableData.shape === shapeInputValue);
+    // if (dateInputValue !== 'null' || cityInputValue !=='null') {
+        
+    //     var filteredData = tableData.filter(tableData => tableData.datetime === dateInputValue);
+
+            
+    //     if (dateInputValue !== 'null' && cityInputValue != 'null') {
+    //              filteredData = tableData.filter((tableData => tableData.datetime === dateInputValue && tableData.city === cityInputValue)); 
+    //         } else if (dateInputValue !== 'null' && cityInputValue === 'null') {
+    //              filteredData = tableData.filter(tableData => tableData.datetime === dateInputValue);
+    //         } else {
+    //             filteredData = tableData.filter(tableData => tableData.city === cityInputValue);
+    //         };
+    // };
   
-    console.log(inputValue);
-    // console.log(people);
-  
-    var dateFilteredData = tableData.filter(tableData => tableData.datetime === inputValue);
-  
-    // remove all rows from table
+ 
     tbody.html("");
 
-    //populate table with filtered data
+    // populate table with filtered data for ufo-level-1 challenge
     dateFilteredData.forEach((ufoReport) => {
         var row = tbody.append("tr");
         Object.entries(ufoReport).forEach(([key, value]) => {
@@ -54,7 +78,41 @@ function runFilter() {
           cell.text(value);
         });
       });
-  };
 
-  var shapeData = tableData.filter(tableData => tableData.shape);
-console.log(shapeData);
+
+    //populate table with filtered data for ufo level-2 challenge
+    // filteredData.forEach((ufoReport) => {
+    //     var row = tbody.append("tr");
+    //     Object.entries(ufoReport).forEach(([key, value]) => {
+    //       var cell = row.append("td");
+    //       cell.text(value);
+    //     });
+    //   });
+
+    };
+
+function runReset() {
+    d3.event.preventDefault();
+    tbody.html("");
+
+    tableData.forEach((ufoReport) => {
+        var row = tbody.append("tr");
+        Object.entries(ufoReport).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });      
+    };
+
+//   if (score > 7) {
+//     goodMovieScores.push(score);
+//   }
+//   // If the movie's rating is between 5 and 7, add it to the list of "Ok" movies
+//   else if (score <= 7 && score > 5) {
+//     okMovieScores.push(score);
+//   }
+//   // Otherwise, if the movie's rating is less than or equal to 5, add it to the list of bad movies
+//   else {
+//     badMovieScores.push(score);
+//   }
+// });
